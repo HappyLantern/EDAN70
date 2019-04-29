ce63a077adefc6c614093bf4cb3dc7c13baade30
El Gutto <ppersson95@gmail.com>
2019-04-29 19:13:30 +0200

    Add processing layers in neural net

--- first_agent.py
+++ first_agent.py
@@ -4,6 +4,7 @@ from pysc2.lib import actions
 from pysc2.lib import features
 from expreplay import ExpReplay
 import numpy
+from utils import preprocess_minimap, preprocess_screen
 
 NUM_SCREEN_FEATURES = len(features.SCREEN_FEATURES)
 NUM_MINIMAP_FEATURES = len(features.MINIMAP_FEATURES)
@@ -27,69 +28,74 @@ class TestAgent():
         self.replay = ExpReplay(10000)
 
         self.minimap = tf.placeholder(shape=[None, NUM_MINIMAP_FEATURES, *MINIMAP_SIZE],
-									  dtype=tf.float32) # TODO: Transpose into [0, 3, 2, 1]?
+                                      dtype=tf.float32)
         self.screen = tf.placeholder(shape=[None, NUM_SCREEN_FEATURES, *SCREEN_SIZE],
-									 dtype=tf.float32)
+                                     dtype=tf.float32)
         self.info = tf.placeholder(shape=[None, NUM_ACTIONS],
-								   dtype=tf.float32)
+                                   dtype=tf.float32)
+        self.screen_processed = preprocess_screen(
+            self.screen)
 
-        mconv1 = layers.conv2d(tf.transpose(self.minimap, [0, 2, 3, 1]),
-                               num_outputs=16,
-                               kernel_size=5,
-                               stride=1,
-                               scope='mconv1')
-        mconv2 = layers.conv2d(mconv1,
-                               num_outputs=32,
-                               kernel_size=3,
-                               stride=1,
-                               scope='mconv2')
-        sconv1 = layers.conv2d(tf.transpose(self.screen, [0, 2, 3, 1]),
-                               num_outputs=16,
-                               kernel_size=5,
-                               stride=1,
-                               scope='sconv1')
-        sconv2 = layers.conv2d(sconv1,
-                               num_outputs=32,
-                               kernel_size=3,
-                               stride=1,
-                               scope='sconv2')
-        info_fc = layers.fully_connected(layers.flatten(self.info),
-                                         num_outputs=256,
-                                         activation_fn=tf.tanh,
-                                         scope='info_fc')
+        self.minimap_processed = preprocess_minimap(
+            self.minimap)
+
+        self.mconv1 = layers.conv2d(tf.transpose(self.minimap, [0, 2, 3, 1]),
+                                    num_outputs=16,
+                                    kernel_size=5,
+                                    stride=1,
+                                    scope='mconv1')
+        self.mconv2 = layers.conv2d(self.mconv1,
+                                    num_outputs=32,
+                                    kernel_size=3,
+                                    stride=1,
+                                    scope='mconv2')
+        self.sconv1 = layers.conv2d(tf.transpose(self.screen, [0, 2, 3, 1]),
+                                    num_outputs=16,
+                                    kernel_size=5,
+                                    stride=1,
+                                    scope='sconv1')
+        self.sconv2 = layers.conv2d(self.sconv1,
+                                    num_outputs=32,
+                                    kernel_size=3,
+                                    stride=1,
+                                    scope='sconv2')
+        self.info_fc = layers.fully_connected(layers.flatten(self.info),
+                                              num_outputs=256,
+                                              activation_fn=tf.tanh,
+                                              scope='info_fc')
 
         # Compute spatial actions
-        feat_conv = tf.concat([mconv2, sconv2], axis=3)
-        spatial_action = layers.conv2d(feat_conv,
-                                       num_outputs=1,
-                                       kernel_size=1,
-                                       stride=1,
-                                       activation_fn=None,
-                                       scope='spatial_action')
-        spatial_action = tf.nn.softmax(layers.flatten(spatial_action))
+        self.feat_conv = tf.concat([self.mconv2, self.sconv2], axis=3)
+        self.spatial_action = layers.conv2d(self.feat_conv,
+                                            num_outputs=1,
+                                            kernel_size=1,
+                                            stride=1,
+                                            activation_fn=None,
+                                            scope='spatial_action')
+        self.spatial_action = tf.nn.softmax(layers.flatten(self.spatial_action))
 
         # Compute non spatial actions and value
-        feat_fc = tf.concat(
-            [layers.flatten(mconv2), layers.flatten(sconv2), info_fc], axis=1)
-        feat_fc = layers.fully_connected(feat_fc,
-                                         num_outputs=256,
-                                         activation_fn=tf.nn.relu,
-                                         scope='feat_fc')
-        non_spatial_action = layers.fully_connected(feat_fc,
-                                                    num_outputs=NUM_ACTIONS,
-                                                    activation_fn=tf.nn.softmax,
-                                                    scope='non_spatial_action')
-        value = tf.reshape(layers.fully_connected(feat_fc,
-                                                  num_outputs=1,
-                                                  activation_fn=None,
-                                                  scope='value'), [-1])
+        self.feat_fc = tf.concat(
+            [layers.flatten(self.mconv2), layers.flatten(self.sconv2), self.info_fc], axis=1)
+        self.feat_fc = layers.fully_connected(self.feat_fc,
+                                              num_outputs=256,
+                                              activation_fn=tf.nn.relu,
+                                              scope='feat_fc')
+        self.non_spatial_action = layers.fully_connected(self.feat_fc,
+                                                         num_outputs=NUM_ACTIONS,
+                                                         activation_fn=tf.nn.softmax,
+                                                         scope='non_spatial_action')
+        self.value = tf.reshape(layers.fully_connected(self.feat_fc,
+                                                       num_outputs=1,
+                                                       activation_fn=None,
+                                                       scope='value'), [-1])
 
-    def step(self, obs):
+    def step(self, timestep):
         """Do action here"""
         self.steps += 1
-        self.reward += obs.reward
+        self.reward += timestep.reward
         function_id = numpy.random.choice(
-            obs.observation.available_actions)
+            timestep.observation.available_actions)
         args = [[numpy.random.randint(0, size) for size in arg.sizes]
                 for arg in self.action_spec.functions[function_id].args]
         return actions.FunctionCall(function_id, args)
